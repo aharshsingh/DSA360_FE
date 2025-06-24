03718580beea7ec765efa66285d6a3c16eb49033
aharshsingh <aharshsingh25@gmail.com>
2025-06-24 16:23:30 +0530

    1st commit

---
+++ my-app/app/app/auth/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { SignInPage } from "@/components/sign-in-flow-1";
+
+const Auth = () => {
+  return (
+    <div className="flex w-full h-screen justify-center items-center">
+      <SignInPage />
+    </div>
+  );
+};
+
+export default Auth;
